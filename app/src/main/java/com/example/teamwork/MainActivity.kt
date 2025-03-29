0ec60dcbfdcd902c2d2eae9a921281c4e2d038e6
Adrian <szponius@gmail.com>
2025-03-29 23:56:51 +0100

    test

---
+++ app/src/main/java/com/example/teamwork/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.teamwork
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_page) // Ustawia widok na main_page.xml
+    }
+}
